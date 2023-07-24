2093448afd2eca9552b9ea8f363d450d3f751f84
Adrian <dreamweaver3d@gmail.com>
2023-07-24 06:35:22 +0000

    vars task 9 commit 1

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int final_digit;
+	int digit;
+
+	final_digit = 9;
+	digit = 0;
+
+	while (digit <= final_digit)
+	{
+		putchar(48 + digit);
+
+		if (digit != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		digit = digit + 1;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
