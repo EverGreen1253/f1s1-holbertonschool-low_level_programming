e310c34667fca2f861d2759c2cbe042a9dde7f3c
Adrian <dreamweaver3d@gmail.com>
2023-07-24 09:35:39 +0000

    funcs task 1 commit 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+int _putchar(char c);
+
+/**
+ * print_alphabet - calls _putchar
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char ch;
+	char final_ch;
+
+	ch = 'a';
+	final_ch = 'z' + 1;
+
+	while (ch != final_ch)
+	{
+		_putchar(ch);
+		ch = ch + 1; 
+	}
+
+	_putchar('\n');
+}
