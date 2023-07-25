8d0cca0d05051b50ce5695a894a6165a4ca063bd
Adrian <dreamweaver3d@gmail.com>
2023-07-25 07:53:52 +0000

    funcs task 3 commit 1

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks if char passed in is lowercase or not.
+ *
+ * Return: 0 or 1.
+ */
+int _islower(char ch)
+{
+	int char_ascii;
+
+	char_ascii = ch;
+
+	if (char_ascii >= 97 && char_ascii <= 122)
+	{
+		return (1);
+	}
+
+	return (0);
+}
