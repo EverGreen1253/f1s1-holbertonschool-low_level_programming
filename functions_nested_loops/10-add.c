76501f7527d3de49917d81993f1e94b6c27a6062
Adrian <dreamweaver3d@gmail.com>
2023-07-27 01:38:45 +0000

    task 10 commit 1

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: addition result of passed in params.
+ */
+int add(int first, int second)
+{
+	return (first + second); 
+}
