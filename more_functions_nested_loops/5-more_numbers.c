71eb0180f86e8e7fb752bb6bc93b6b40dadd86cc
Adrian <dreamweaver3d@gmail.com>
2023-07-29 03:58:48 +0000

    funcs2 task 5 commit 1

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ *
+ * Return: rows of numbers.
+ */
+void more_numbers(void)
+{
+	int row = 0;
+	int col = 0;
+
+	while (row < 10)
+	{
+		while (col < 15)
+		{
+			if (col >= 10) {
+				_putchar(((col - (col % 10)) / 10) + 48);
+			}
+			_putchar((col % 10) + 48);
+			col = (col + 1); 
+		}
+
+		col = 0;
+		row = row + 1;
+		_putchar('\n');
+	}
+}
