853c7789a52f4bfd3f9d95a86b5574b1907508c5
Adrian <dreamweaver3d@gmail.com>
2023-07-29 03:05:49 +0000

    funcs2 task 2 commit 1

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - check the code
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
