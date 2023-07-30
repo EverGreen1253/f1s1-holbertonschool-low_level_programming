74d322fc6007cdac2687eba41b6102b55baf0f40
Adrian <dreamweaver3d@gmail.com>
2023-07-30 05:55:15 +0000

    pointers task 5 commit 1

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - check the code
+ * @s: pointer to the string to reverse
+ *
+ * Return: reverses the string passed in.
+ */
+void rev_string(char *s)
+{
+	int count = 0;
+	int length;
+	char temp;
+
+	while (s[count] != '\0')
+	{
+		count = count + 1;
+	}
+
+	length = count - 1;
+
+	while (count > (length / 2))
+	{
+		count = count - 1;
+		temp = s[count];
+		s[count] = s[length - count];
+		s[length - count] = temp;
+
+	}
+}
