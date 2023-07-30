4270a13721fa25cf6b91420253d14d54349132c9
Adrian <dreamweaver3d@gmail.com>
2023-07-30 02:52:13 +0000

    pointers task 2 commit 1

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - check the code
+ * @s: the pointer to the start of the string
+ *
+ * Return: length of the string passed in.
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+	while(s[count] != '\0')
+	{
+		count = count + 1;
+	}
+	return (count);
+}
