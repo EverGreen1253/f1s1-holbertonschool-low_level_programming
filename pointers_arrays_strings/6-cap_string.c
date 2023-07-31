fd26c0be9628b905cfb9bb68e1325c8de86cfeac
Adrian <dreamweaver3d@gmail.com>
2023-07-31 06:43:05 +0000

    pointers2 task 6 commit 1

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * *cap_string - check the code
+ * @c: pointer to string first elem
+ *
+ *Pattern recognition time! Capitalise at start of string ot when the prev char is not a letter
+ *
+ * Return: pointer to the string.
+ */
+char *cap_string(char *c)
+{
+	int capnext = 1;
+	int count = 0;
+	int letter;
+	int prevchar;
+
+	while (c[count] != '\0')
+	{
+		if (count > 0)
+		{
+			prevchar = c[count - 1];
+			if ((capnext == 0) && (prevchar < 65))
+			{
+				capnext = 1;
+			
+			}
+			else
+			{
+				capnext = 0;
+			}
+		}
+
+		letter = c[count];
+
+		if (letter >= 97 && letter <= 122)
+		{
+			if (capnext == 1)
+			{
+				c[count] = letter - 32;
+				capnext = 0;
+			}
+		}
+
+		count = count + 1;
+	}
+
+	return c;
+}
