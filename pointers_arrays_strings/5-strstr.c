567c40078a69653638c951fcbd3117e850a06a7b
Adrian <dreamweaver3d@gmail.com>
2023-08-01 04:29:57 +0000

    pointers3 task 5 commit 1

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - check the code
+ * @haystack: the string to search through
+ * @needle: the exact string to match
+ *
+ * Return: pointer to substring location.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+	int match = 0;
+
+	while (haystack[i] != '\0')
+	{
+		if (haystack[i] == needle[0])
+		{
+			match = 1;
+			while (match == 1 && needle[j] != '\0' && haystack[i + j] != '\0')
+			{
+				if (needle[j] != haystack[i + j])
+				{
+					match = 0;
+				}
+				j = j + 1;
+			}
+
+			/*
+			 * comparison while loop finished without setting match to 0
+			 */
+			if (match == 1)
+			{
+				return (haystack + i);
+			}
+		}
+
+		i = i + 1;
+		j = 0;
+	}
+
+	return (NULL);
+}
