b97e98f9a6716fb9a6bf91a5091861ccda7c2600
Adrian <dreamweaver3d@gmail.com>
2023-08-01 01:37:31 +0000

    pointers3 task 2 commit 1

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - check the code
+ * @s: the string to be scanned
+ * @c: the character to find
+ *
+ * Return: pointer to location of char in scanned string.
+ */
+char *_strchr(char *s, char c)
+{
+	int count = 0;
+	int found = 0;
+
+	while ((s[count] != '\0') && (found == 0))
+	{
+		if (s[count] == c)
+		{
+			found = 1;
+		}
+		count = count + 1;
+	}
+
+	return (s + (count - 1));
+}
