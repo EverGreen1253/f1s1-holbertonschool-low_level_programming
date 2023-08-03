27a819af07f4e923ca1ba6822fdb35ae70b7751a
Adrian <dreamweaver3d@gmail.com>
2023-08-03 08:05:02 +0000

    libraries task 0 commit 2

--- static_libraries_store/main.c
+++ static_libraries_store/main.c
@@ -1,7 +1,21 @@
 #include "main.h"
 
+/*
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
+*/
+
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
 	return (0);
 }
