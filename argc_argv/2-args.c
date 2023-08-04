15524a2aa70e239eb9a800566a1027db35656e28
Adrian <dreamweaver3d@gmail.com>
2023-08-04 04:06:32 +0000

    argc task 2 commit 1

---
+++ argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: no. of params
+ * @argv: array of params
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	if (argc > 0 && argv[0] != NULL)
+	{
+		while (i < argc)
+		{
+			printf("%s\n", (argv[i]));
+
+			i = i + 1; 
+		}
+	}
+	return (0);
+}
