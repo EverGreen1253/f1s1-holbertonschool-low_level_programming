4f07c8099d6ecbc72c68d8c9f27636d14d03c522
Adrian <dreamweaver3d@gmail.com>
2023-08-05 05:04:43 +0000

    malloc task 2 commit 5

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,34 +16,39 @@ char *str_concat(char *s1, char *s2)
 	int size;
 	char *s;
 
-	if (s1 == NULL || s2 == NULL)
-	{
-		return (NULL);
-	}
-	while (s1[i] != '\0')
-	{
-		i = i + 1;
-	}
-	while (s2[j] != '\0')
-	{
-		j = j + 1;
-	}
+	if (s1 != NULL)
+		while (s1[i] != '\0')
+		{
+			i = i + 1;
+		}
+	if (s2 != NULL)
+		while (s2[j] != '\0')
+		{
+			j = j + 1;
+		}
 	size = (i + 1) + (j + 1) - 1;
 	s = malloc(size);
-	if (s == NULL) {
+	if (s == NULL)
+	{
 		return (NULL);
 	}
 	i = j = 0;
-	while (s1[i] != '\0')
+	if (s1 != NULL)
 	{
-		s[i] = s1[i];
-		i = i + 1;
+		while (s1[i] != '\0')
+		{
+			s[i] = s1[i];
+			i = i + 1;
+		}
 	}
-	while (s2[j] != '\0')
+	if (s2 != NULL)
 	{
-		s[j + i] = s2[j];
-		j = j + 1;
+		while (s2[j] != '\0')
+		{
+			s[j + i] = s2[j];
+			j = j + 1;
+		}
 	}
-	s[j + i] = '\0';
+	s[(i + 1) + (j + 1) - 1] = '\0';
 	return (s);
 }
