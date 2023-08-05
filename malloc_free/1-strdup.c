7d0b86830fc81da8919ab84658af9a5b9bd37810
Adrian <dreamweaver3d@gmail.com>
2023-08-05 03:44:57 +0000

    malloc task 1 commit 4

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -13,7 +13,12 @@ char *_strdup(char *str)
 	int i = 0;
 	char *s;
 
-	while (str[i] != '\0' && *str != 0 && str != NULL)
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[i] != '\0' && *str != 0)
 	{
 		i = i + 1;
 	}
