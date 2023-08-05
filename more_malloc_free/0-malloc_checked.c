1b62149bd77773860eb991fbc62b99e6f0b68d81
Adrian <dreamweaver3d@gmail.com>
2023-08-05 15:16:48 +0800

    malloc2 task 0 commit 1

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - check the code
+ * @b: size of memory space to reserve
+ *
+ * Return: pointer address to allocated memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+	p = malloc(b);
+	if (p == NULL)
+	{
+		exit (98);
+	}
+
+	return (p);
+}
