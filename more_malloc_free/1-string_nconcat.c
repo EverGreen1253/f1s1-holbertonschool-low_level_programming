9cf59b6160d1185c02e2d78117c28eb5cab7226d
Adrian <dreamweaver3d@gmail.com>
2023-08-05 16:21:19 +0800

    malloc2 task1 commit 4

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	size = i + n + 1;
 
-	s = malloc(size);
+	s = calloc(size, 1);
 	if (s == NULL)
 	{
 		return (NULL);
@@ -53,6 +53,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			j = j + 1;
 		}
 	}
-	s[(i + n)] = '\0';
+	s[size - 1] = '\0';
 	return (s);
 }
