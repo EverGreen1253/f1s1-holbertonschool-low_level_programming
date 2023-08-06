08931859541222fa065905415c7c5972df71e426
Adrian <dreamweaver3d@gmail.com>
2023-08-06 08:30:24 +0800

    malloc2 task 3 commit 1

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * array_range - check the code
+ * @min: min value
+ * @max: max value
+ *
+ * Return: pointer to min-max array.
+ */
+int *array_range(int min, int max)
+{
+	int i = 0;
+	int size;
+	int *s;
+
+	if (min > max) {
+		return (NULL);
+	}
+
+	size = max - min + 1;
+	s = malloc(size * sizeof(int));
+
+	while (i < size)
+	{
+		s[i] = min + i;
+		i = i + 1;
+	}
+
+	return (s);
+}
