4589768b172199152f468f8e86d07844bc997413
Adrian <dreamweaver3d@gmail.com>
2023-08-06 12:00:22 +0800

    struct task 0 commit 2

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog 
+/**
+ * struct dog - Doggo
+ * @name: pointer to string
+ * @age: age is non-integer
+ * @owner: pointer to string
+ */
+struct dog
 {
 	char *name;
 	float age;
