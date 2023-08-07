a3afd305be45860b1e4ef3486fb9c7eea77e0b54
Adrian <dreamweaver3d@gmail.com>
2023-08-07 12:06:37 +0800

    funcpoint task 1 commit 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - check the code
+ * @array: array of int
+ * @size: size of array
+ * @action: function to call
+ *
+ * Return: nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	while (i < size)
+	{
+		action(array[i]);
+		i = i + 1;
+	}
+}
