de0defdf1315f943fd30d09dcf1541f91466b1b0
Adrian <dreamweaver3d@gmail.com>
2023-08-07 12:21:56 +0800

    funcpoint task 2 commit 1

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - check the code
+ * @array: the array passed in
+ * @size: the size of the array passed in
+ * @cmp: function to run on the array passed in
+ *
+ * Return: index where the wanted int is located in the array.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0 && array != NULL && cmp != NULL)
+	{
+		return (-1);
+	}
+
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+		i = i + 1;
+	}
+
+	return (-1);
+}
+
