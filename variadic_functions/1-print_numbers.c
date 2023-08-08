bf0cbf9ccc1f08682c8b82266bfae2135d14302d
Adrian <dreamweaver3d@gmail.com>
2023-08-08 08:49:11 +0800

    variadic task 1 commit 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - check the code
+ * @separator: delimiter character
+ * @n: num of params passed in
+ *
+ * Return: nothing.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i = 0;
+	int term;
+
+	if (n != 0)
+	{
+		va_start(list, n);
+			while (i < n)
+			{
+				term = va_arg(list, int);
+				printf("%d", term);
+
+				if (i != n - 1 && separator != NULL)
+				{
+					printf("%s", separator);
+				}
+
+				i = i + 1;
+			}
+			printf("\n");
+		va_end(list);
+	}
+}
