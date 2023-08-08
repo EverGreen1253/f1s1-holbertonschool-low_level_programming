9cfd3adce4baa82a6360a4e68a402229955597c2
Adrian <dreamweaver3d@gmail.com>
2023-08-08 13:20:42 +0800

    variadic task 3 commit 1

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,120 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - check the code
+ * @format: string containing all the format chars
+ *
+ * Return: nothing.
+ */
+void print_all(const char * const format, ...)
+{
+	op_t ops[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
+
+	va_list list;
+	int i = 0;
+	int j = 0;
+	int printed_one = 0;
+
+	va_start(list, format);
+		while (format[i] != '\0')
+		{
+			/**
+			 * OK great, we managed to extract the character
+			 * now we need to see if it's inside the ops
+			 *
+			 * printf("%c, ", format[i]);
+			 */
+
+			while (j < 4)
+			{
+				if (*ops[j].t == format[i])
+				{
+					/**
+					 * printf("match found - %c\n", format[i]);
+					 */
+
+					if (printed_one == 1)
+					{
+						printf(", ");
+					}
+
+					ops[j].f(&list);
+					printed_one = 1;
+				}
+				j = j + 1;
+			}
+
+			j = 0;
+			i = i + 1;
+		}
+	va_end(list);
+
+	printf("\n");
+}
+
+/**
+ * print_char
+ * @list: pointer to list
+ *
+ * Return : nothing
+ */
+void print_char(va_list *list)
+{
+	char c;
+
+	c = va_arg(*list, int);
+
+	printf("%c", c) ;
+}
+
+/**
+ * print_int
+ * @list: pointer to list
+ *
+ * Return : nothing
+ */
+void print_int(va_list *list)
+{
+	int x;
+
+	x = va_arg(*list, int);
+
+	printf("%d", x);
+}
+
+/**
+ * print_float
+ * @list: pointer to list
+ *
+ * Return : nothing
+ */
+void print_float(va_list *list)
+{
+	double f;
+
+	f = va_arg(*list, double);
+
+	printf("%f", f);
+}
+
+/**
+ * print_string
+ * @list: pointer to list
+ *
+ * Return : nothing
+ */
+void print_string(va_list *list)
+{
+	char *s;
+
+	s = va_arg(*list, char *);
+
+	printf("%s", s);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,29 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *t;
+	void (*f)(va_list *list);
+} op_t;
+
+void print_char(va_list *list);
+void print_int(va_list *list);
+void print_float(va_list *list);
+void print_string(va_list *list);
 
 #endif
