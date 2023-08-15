9a40f024a614b27339ef469855cdec61a9bd5349
Adrian <dreamweaver3d@gmail.com>
2023-08-15 15:01:17 +0800

    bitm task 4 commit 1

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * clear_bit - check the code
+ * @n: pointer to the number
+ * @index: the bit position
+ *
+ * Return: 1 or -1.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int result = -1;
+	unsigned long int value = *n;
+	int r;
+	signed long int i = 0;
+
+	if (index > 65)
+		return (-1);
+
+	while (value > 0)
+	{
+		r = (value % 2);
+		if (r == 1)
+			value = value - 1;
+
+		if (i == index)
+		{
+			/* only bother if bit is 1 */
+			if (r == 1)
+			{
+				*n = *n - twopower(i);
+			}
+
+			return (1);
+		}
+
+		i = i + 1;
+		value = value / 2;
+	}
+
+	return (result);
+}
+
+/**
+ * twopower - my own implementation of the pow function
+ * @power: power
+ *
+ * Return: int result.
+ */
+unsigned long int twopower(signed long int power)
+{
+	unsigned long int result = 1;
+	signed long int i = 0;
+
+	while (i < power)
+	{
+		result = result * 2;
+		i = i + 1;
+	}
+
+	/* printf("base - 2, power - %ld, result - %lu\n", power, result); */
+
+	return (result);
+}
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -8,5 +8,6 @@ void print_binary(unsigned long int n);
 unsigned long int twopower(signed long int power);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 #endif
