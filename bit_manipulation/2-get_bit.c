ff919e3052bc60a059581f065a97a7b4c97e5a28
Adrian <dreamweaver3d@gmail.com>
2023-08-15 12:32:12 +0800

    bitm task 2 commit 1

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * get_bit - check the code
+ * @n: the number
+ * @index: the bit position
+ *
+ * Return: bit value or -1.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int result = -1;
+	unsigned long int value = n;
+	int r;
+	unsigned int i = 0;
+
+	while (value != 0)
+	{
+		r = (value % 2);
+		if (r == 1)
+		{
+			value = value - 1;
+			/* printf("i - %d, bit - 1\n", i); */
+		}
+		else
+		{
+			/* printf("i - %d, bit - 0\n", i); */
+		}
+
+		if (i == index)
+		{
+			return (r);
+		}
+
+		i = i + 1;
+		value = value / 2;
+	}
+
+	return (result);
+}
--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -6,5 +6,6 @@ unsigned int binary_to_uint(const char *b);
 int _pow(int base, int power);
 void print_binary(unsigned long int n);
 unsigned long int twopower(signed long int power);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
