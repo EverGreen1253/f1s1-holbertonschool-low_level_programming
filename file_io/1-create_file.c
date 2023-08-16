78a48fd23c29e9a859a8dda7db014089444b77bc
Adrian <dreamweaver3d@gmail.com>
2023-08-16 23:54:30 +0800

    fileio task 1 commit 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * create_file - check the code
+ * @filename: string of filename
+ * @text_content: text to write
+ *
+ * Return: 1 or -1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	int i = 0;
+
+	if (fd < 0)
+	{
+		return (-1);
+	}
+
+	while (text_content[i] != '\0')
+	{
+		i = i + 1;
+	}	
+
+	write(fd, text_content, (i - 1));
+
+	if (close(fd) < 0)
+	{
+		return (-1);
+	}
+
+	return (1);
+}
