c923456f75ef1c7ee8f541190c3b3ee5b2e284aa
Adrian <dreamweaver3d@gmail.com>
2023-08-16 17:11:24 +0800

    fileio task 0 commit 1

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * read_textfile - check the code
+ * @filename: name of file
+ * @letters: num of letters to read and print
+ *
+ * Return: num of letters read and printed.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t result = 0;
+	int fd = open(filename, O_RDONLY), i = 0, sz;
+	char *c = malloc(letters);
+
+	if (filename == NULL || (fd < 0) || c == NULL)
+	{
+		return (0);
+	}
+
+	sz = read(fd, c, letters);
+	c[sz] = '\0';
+
+	while (c[i] != '\0')
+	{
+		_putchar(c[i]);
+		i = i + 1;
+	}
+	result = i;
+
+	return (result);
+}
