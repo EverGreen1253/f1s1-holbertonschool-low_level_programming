480795043531a990b1cd9c2ef820442671248377
Adrian <dreamweaver3d@gmail.com>
2023-08-16 23:57:09 +0800

    fileio task 1 commit 2

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	int fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	int i = 0;
 
-	if (fd < 0)
+	if (fd < 0 || text_content == NULL)
 	{
 		return (-1);
 	}
