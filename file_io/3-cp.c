741226934c7ca40cee2b6e9a2ff9aa68d75bfae7
Adrian <dreamweaver3d@gmail.com>
2023-08-17 17:17:58 +0800

    fileio task 3 commit 1

---
+++ file_io/3-cp.c
@@ -0,0 +1,64 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - check the code
+ * @argc: no. of params
+ * @argv: array of params
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	char *fone, *ftwo;
+	int fdone, fdtwo, sz = 1024, *buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	else
+	{
+		fone = argv[1];
+		ftwo = argv[2];
+		/* printf("command - %s, file one - %s, file two - %s\n", argv[0], fone, ftwo); */
+
+		fdone = open(fone, O_RDONLY);
+		fdtwo = open(ftwo, O_RDWR | O_CREAT | O_TRUNC, 0664);
+
+		if (fdone < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s", fone);
+			exit(98);
+		}
+		if (fdtwo < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s", ftwo);
+			exit(99);
+		}
+
+		while (sz != 0)
+		{
+			sz = read(fdone, buffer, 1024);
+			write(fdtwo, buffer, sz);
+		}
+
+		if (close(fdone) < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdone);
+			exit(100);
+		}
+
+		if (close(fdtwo) < 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d", fdtwo);
+			exit(100);
+		}
+
+	}
+	return (0);
+}
