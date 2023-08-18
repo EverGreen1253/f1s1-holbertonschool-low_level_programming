2417325956ad53b9e8844211c35f1d969a0cb65e
Adrian <dreamweaver3d@gmail.com>
2023-08-18 14:54:30 +0800

    hash task 0 commit 1

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,14 +11,24 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_node_t *table;
-	hash_table_t *table_pointer;
+	hash_node_t *node_array;
+	hash_table_t *table;
+	
+	/* printf("sizeof hash_table_t - %lu\n", sizeof(hash_table_t)); */
+	/* printf("sizeof hash_node_t - %lu\n", sizeof(hash_node_t *)); */
 
-	table_pointer = malloc(sizeof(hash_table_t *) * 1);
-	table = malloc(sizeof(hash_node_t *) * size);
+	table = malloc(sizeof(hash_table_t) * 1);
+	node_array = malloc(sizeof(hash_node_t *) * size);
 
-	table_pointer->size = size;
-	table_pointer->array = &table;
+	if (table == NULL || node_array == NULL)
+	{
+		free(table);
+		free(node_array);
+		exit(98);
+	}
 
-	return table_pointer;
+	table->size = size;
+	table->array = &node_array;
+
+	return table;
 }
