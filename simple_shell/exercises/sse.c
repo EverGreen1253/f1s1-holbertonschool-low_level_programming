2df997bc37c59ea7a7b628c697ff92819077be8b
Adrian <dreamweaver3d@gmail.com>
2023-08-21 07:19:52 +0800

    simple shell exercises

---
+++ simple_shell/exercises/sse.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * main - fork & wait example
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    return (0);
+}
