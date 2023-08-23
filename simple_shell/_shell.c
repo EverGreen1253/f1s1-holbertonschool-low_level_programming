17b99a2b62128c7ff713f9918c67c381053cdf37
Adrian <dreamweaver3d@gmail.com>
2023-08-23 16:41:22 +0800

    command prompt works

--- simple_shell/_shell.c
+++ simple_shell/_shell.c
@@ -2,6 +2,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
+#include "main.h"
+
 
 /**
  * main - simple shell! The big one!
@@ -10,34 +13,87 @@
  */
 int main(void)
 {
-	int count = 0;
-	char *buffer = NULL, *token, *temp;
+	int i = 0, count = 0;
+	char *buffer = NULL, *token, *temp = NULL, *cmd;
 	size_t bufsize = 65535;
 	size_t typed;
-	char **argv;
+	char **argv = NULL;
 
-	printf("$ ");
-	typed = getline(&buffer, &bufsize, stdin);
-	// printf("%zu characters were read.\n", typed);
-
-	temp = malloc(strlen(buffer) + 1);
-	if (malloc == NULL)
+	while (1)
 	{
-		exit(98);
+		printf("$ ");
+		typed = getline(&buffer, &bufsize, stdin);
+		// printf("%zu characters were read.\n", typed);
+
+		temp = malloc(strlen(buffer) + 1);
+		if (temp == NULL)
+		{
+			free(temp);
+			exit(98);
+		}
+		strcpy(temp, buffer);
+
+		/* we count how many items there are first */
+		token = strtok(temp, " ");
+		while (token != NULL)
+		{
+			count++;
+			token = strtok(NULL, " ");
+		}
+		free(temp);
+
+		//printf("You typed: %s\n",buffer);
+		//printf("count - %d\n", count);
+
+		// let's load up the argv array
+		argv = malloc(sizeof(char *) * count + 1);
+		if (argv == NULL)
+		{
+			free(argv);
+			exit(98);
+		}
+
+		token = strtok(buffer, " ");
+		i = 0;
+		while (token != NULL)
+		{
+			temp = malloc(strlen(token) + 1);
+			if (temp == NULL)
+			{
+				free(temp);
+				exit(98);
+			}
+
+			// change the newline into a null character
+			strcpy(temp, token);
+			temp[strcspn(temp, "\n")] = '\0';
+			argv[i] =  temp;
+
+			token = strtok(NULL, " ");
+			i = i + 1;
+		}
+		argv[i] = NULL;
+
+		pid_t pid = fork();
+		if (pid < 0)
+		{
+			// handle error
+		}
+		else if (pid == 0)
+		{
+			execvp(argv[0], argv);
+			// if execvp returns, there was an error
+			perror("execvp");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			wait(NULL);
+		}
 	}
-	strcpy(temp, buffer);
+	free(buffer);
 
-	token = strtok(temp, " ");
-	while (token != NULL)
-	{
-		count++;
-		token = strtok(NULL, " ");
-	}
-
-	// printf("You typed: %s\n",buffer);
-	// printf("count - %d\n", count);
-
-	argv = malloc(sizeof(char *) * count + 1);
-
-	return(0);
+	return (0);
 }
+
+
