9e2264c31282b2a5727f71c57ec4d439ec80c927
Adrian <dreamweaver3d@gmail.com>
2023-10-27 11:30:01 +0800

    updated pointers project 3 folder name

--- pointers_arrays_strings_1/1-main.c
+++ pointers_arrays_strings_1/1-main.c
@@ -8,14 +8,13 @@
  */
 int main(void)
 {
-	char *s = "hello";
-	char *f;
+	int a;
+	int b;
 
-	f = _strchr(s, 'l');
-
-	if (f != NULL)
-	{
-		printf("%s\n", f);
-	}
+	a = 98;
+	b = 42;
+	printf("a=%d, b=%d\n", a, b);
+	swap_int(&a, &b);
+	printf("a=%d, b=%d\n", a, b);
 	return (0);
 }
